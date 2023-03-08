6ca9deb1a3aec784735147042c2be75a408c3e9d
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-03-08 12:17:18 +0700

    first commit

---
+++ Latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){ //begin
+
+  //Numeric Luas,P,L
+  double Luas, P,L;
+  //display "Masukkan Panjangnya = "
+  cout << "Masukkan Panjangnya = ";
+  //Accept P
+  cin >> P ;
+   //display "Masukkan Lebarnya = "
+  cout << "Masukkan Lebarnya = ";
+   //Accept L
+  cin >> L ;
+  //compute Luas = P*L
+  Luas = P*L;
+  //display "Luasnya = " + Luas
+  cout << "Luasnya = " << Luas;
+ } //end
